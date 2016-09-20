d59cbdc64520c0e51c074a8f712906c1f4831536
learn <learn@learn06.local>
2016-09-20 13:51:56 -0700

    added map of all apts on index

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,4 +1,13 @@
 class Apartment < ActiveRecord::Base
-  geocoded_by :street1
+
+  def full_address
+    @street = street1
+    @city = city
+    @postalcode = postalcode
+    @full_address = @street + " " + @city + " " + @postalcode
+  end
+
+  geocoded_by :full_address
   after_validation :geocode
+
 end
