43349541910ff51a7ad2e45d3c7a97954c6a2428
sophie and amy <sophie.la.merkley@gmail.com>
2016-09-21 10:31:04 -0700

    backup before adding devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
 
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "50x50#" }
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "50x50#" },
+  :default_url => "default_image_:style.jpeg"
+
  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   has_many :apartments
+
 end
